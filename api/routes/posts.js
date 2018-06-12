a81e993342183915e1d1093b385acd54615e5369
gopigoppu <gopi.learner93@gmail.com>
2018-06-12 10:59:56 +0530

    file upload functionality add with multer npm package

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,12 +1,45 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const multer = require('multer');
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads/');
+    },
+    filename: function (req, file, cb) {
+        // console.log(new Date().getTime() + '-' + file.originalname);
+        cb(null, new Date().getTime() + '-' + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        //accept a file
+        cb(null, true);
+    } else {
+        // reject a file
+        cb(new Error('File not supported'), false);
+    }
+
+
+
+}
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+});
 
 const Post = require('../models/post');
 
 router.get('/', (req, res, next) => {
     Post.find()
-        .select('post_title post_content')
+        .select('post_title post_content post_image')
         .exec()
         .then(docs => {
             const response = {
@@ -15,6 +48,7 @@ router.get('/', (req, res, next) => {
                     return {
                         post_title: doc.post_title,
                         post_content: doc.post_content,
+                        post_image: doc.post_image,
                         _id: doc._id,
                         resuest: {
                             type: 'GET',
@@ -32,11 +66,13 @@ router.get('/', (req, res, next) => {
         });
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', upload.single('postImage'), (req, res, next) => {
+    console.log(req.file);
     const post = new Post({
         _id: new mongoose.Types.ObjectId(),
         post_title: req.body.post_title,
-        post_content: req.body.post_content
+        post_content: req.body.post_content,
+        post_image: req.file.path
     });
     post.save()
         .then(result => {
--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const postRoutes = require('./api/routes/posts');
 const commentRoutes = require('./api/routes/comments');
 
 app.use(morgan('dev'));
+app.use('/uploads', express.static('uploads'))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
